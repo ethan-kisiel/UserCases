fa7c02f99d54cd16c560a83ac9dee8ae5db159b6
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-06-28 20:13:54 -0400

    Initial Commit

---
+++ UserCases/UserCasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserCasesApp.swift
+//  UserCases
+//
+//  Created by Ethan Kisiel on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct UserCasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
